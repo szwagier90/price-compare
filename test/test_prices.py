791ce31cb5fd1edb79a82e73900e3c86512b100e
Szwagier <szwagier90@gmail.com>
2018-01-04 23:32:07 +0000

    Test unittest

---
+++ test/test_prices.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
